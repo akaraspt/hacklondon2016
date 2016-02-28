a4d73bdec99f1453ff34f9cd41efa38275ae8b70
Chiraphat Chaiphet <chiraphat@gmail.com>
2016-02-28 01:26:30 +0000

    update detectface

--- who-is-that-guy-vsproject/who-is-that-guy/Controllers/DetectfaceController.cs
+++ who-is-that-guy-vsproject/who-is-that-guy/Controllers/DetectfaceController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace who_is_that_guy.Controllers
@@ -10,19 +11,27 @@ namespace who_is_that_guy.Controllers
     public class DetectfaceController : ApiController
     {
 
-        // POST: api/Detectface
-        public void Post([FromBody]string value)
+        [Route("api/Detectface")]
+        [HttpPost]
+        public string[] ImageUpload()
         {
+            var request = HttpContext.Current.Request;
+            var filePath = "C:\\Temp\\" + request.Headers["filename"];
+            using (var fs = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
+            {
+                request.InputStream.CopyTo(fs);
+            }
+
+
+
+            //Detection code using FaceAPI here
+
+
+
+
+
+            return new string[] { "First User", "Second User" };
         }
 
-        // PUT: api/Detectface/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Detectface/5
-        public void Delete(int id)
-        {
-        }
     }
 }
